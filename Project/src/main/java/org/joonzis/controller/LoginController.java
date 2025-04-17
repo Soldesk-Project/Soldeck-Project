d0966610f9bc55aa7f9df1af810d30e37bcde21e
sdedu <sdedu@DESKTOP-F8L4K7P>
2025-04-17 18:59:31 +0900

    header -> 로그인 페이지로 이동 하는 거 수정 loginController 위 동일

--- Project/src/main/java/org/joonzis/controller/LoginController.java
+++ Project/src/main/java/org/joonzis/controller/LoginController.java
@@ -5,16 +5,26 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/views/");
+	    resolver.setSuffix(".jsp");
+	    return resolver;
+	}
 	
 	@RequestMapping(value = "/loginPage", method = RequestMethod.GET)
 	public String loginPage() {
