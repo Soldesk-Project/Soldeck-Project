e602f7a28aabd2874baaf930c70b276b8d449d39
l2cr <coco7537@naver.com>
2025-05-08 14:26:53 +0900

    미니게임 추가

--- Project/src/main/java/org/joonzis/controller/EventController.java
+++ Project/src/main/java/org/joonzis/controller/EventController.java
@@ -1,6 +1,10 @@
 package org.joonzis.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.joonzis.domain.MemberVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +16,12 @@ import lombok.extern.log4j.Log4j;
 public class EventController {
 
 	@GetMapping("/main")
-	public String main() {
+	public String main(Model model, HttpSession session) {
+		MemberVO loggedInMember = (MemberVO) session.getAttribute("loggedInUser");
+		if (loggedInMember == null) {
+			// 로그인되지 않은 경우 처리 (예: 로그인 페이지로 리다이렉트)
+			return "redirect:/login/loginPage";
+		}
 		log.info("main...");
 		return "/event/main";
 	}
